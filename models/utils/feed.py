f1b0ffabfb3b1da8fd8b4b29d071abfc6c115015
Leandro Toledo <leandrotoledodesouza@gmail.com>
2018-11-11 08:43:47 -0500

    Update feed.py
    Fixes uncaught exceptions when casting posts

--- models/utils/feed.py
+++ models/utils/feed.py
@@ -17,10 +17,15 @@ class Posts(object):
         for link in soup.select('#threads a.title'):
             post = dict()
 
-            post['title'] = link.text
-            post['href'] = link.get('href')
-            post['uid'] = int(post['href'].replace(url, '')[:6])  # TODO
+            try:
+                post['title'] = link.text
+                post['href'] = link.get('href')
+                post['uid'] = int(post['href'].replace(url, '')[:6])  # TODO
 
-            posts.append(post)
+                posts.append(post)
+            except Exception as e:
+                print(e)
+
+                pass
 
         return posts
